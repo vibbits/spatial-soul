71cf21ae684fdb6c8025a206ba67fa4bd96b9999
Frank Vernaillen <frank.vernaillen@ugent.be>
2021-09-07 14:52:41 +0200

    Bump up version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="spatial-soul",
-    version="0.1.2",
+    version="0.1.3",
     description="Spatial -omics utilities library",
     long_description=long_description,
     long_description_content_type="text/markdown",
